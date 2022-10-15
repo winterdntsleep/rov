28197c435b3a04e698846f1c3493beeda5ff4d1d
winterdntsleep <112240206+winterdntsleep@users.noreply.github.com>
2022-10-15 15:51:26 +0700

    Add files via upload

---
+++ src/pages/Home.vue
@@ -0,0 +1,368 @@
+<template>
+  <div>
+    <Navbar />
+   
+   
+    <div class="hero v-h-full overlay">
+    
+<div class="container-fluid">
+    <img src="images\bo5.gif" class="width-complete" alt="...">
+</div>
+      <div class="container">
+        <div class="row align-items-center v-h-full">
+          <div class="col-lg-10 col-xxl-6">
+            <h1 class="heading-2 text-white mb-5" data-aos="fade-up">
+              Arena of valor
+            </h1>
+            <h2 class="heading-2 text-white mb-3" data-aos="fade-up">
+              "S-Dreamer"
+            </h2>
+            <p data-aos="fade-up" class="d-flex" data-aos-delay="200">
+              <a
+                href="portfolio.vue"
+                class="btn btn-primary-outline white me-4"
+                >ROV</a
+              >
+              <a
+                href="https://www.youtube.com/watch?v=S9maWf8Nm_k"
+                class="glightbox d-inline-flex align-items-center playvid"
+                ><span class="icon-play me-2"></span
+                ><span>S-Dreamer</span></a
+              >
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <audio controls autoplay>
+      <source src="coolkid.mp3" type="audio/mpeg" />
+      Your browser does not support the audio element.
+    </audio>
+
+    <div class="section bg-light">
+      <div class="container">
+        <div class="row">
+          <div
+            class="col-lg-4 mb-4 mb-lg-0"
+            data-aos="fade-up"
+            data-aos-delay="0"
+          >
+            <div class="service grayscale">
+              <div class="service-img">
+                <a href="#"
+                  ><img src="images/hel.jpg" alt="Image" class="img-fluid"
+                /></a>
+              </div>
+              <div class="service-inner">
+                <span></span>
+                <h3>Afata Forest | RoV Comic</h3>
+                <p>
+                Story about Helan
+                </p>
+                <p class="mb-0"><a href="#" class="more">Learn more</a></p>
+              </div>
+            </div>
+          </div>
+          <div
+            class="col-lg-4 mb-4 mb-lg-0"
+            data-aos="fade-up"
+            data-aos-delay="100"
+          >
+            <div class="service grayscale">
+              <div class="service-img">
+                <a href="#"
+                  ><img src="images/uou.jpg" alt="Image" class="img-fluid"
+                /></a>
+              </div>
+              <div class="service-inner">
+                <span></span>
+                <h3>《RoV》X《Demon Slayer: Kimetsu no Yaiba》</h3>
+                <p>
+                  Tanjiro Kamado and Nezuko Kamado in RoV now!
+                </p>
+                <p class="mb-0"><a href="#" class="more">Learn more</a></p>
+              </div>
+            </div>
+          </div>
+          <div
+            class="col-lg-4 mb-4 mb-lg-0"
+            data-aos="fade-up"
+            data-aos-delay="200"
+          >
+            <div class="service grayscale">
+              <div class="service-img">
+                <a href="#"
+                  ><img src="images/new.png" alt="Image" class="img-fluid"
+                /></a>
+              </div>
+              <div class="service-inner">
+                <span></span>
+                <h3>New update!</h3>
+                <p>
+                  In this update, which heroes and items have been adjusted, let's have a look!
+                </p>
+                <p class="mb-0"><a href="#" class="more">Learn more</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="section">
+      <div class="container">
+        <div class="row justify-content-center mb-5">
+          <div
+            class="col-lg-6 text-center"
+            data-aos="fade-up"
+            data-aos-delay="100"
+          >
+            <span class="subheading mb-2 d-inline-block"
+              >13/10/2022</span
+            >
+            <h2 class="mb-4">Free hero of the week</h2>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="container-fluid">
+            <div class="row">
+              <div
+                class="col-md-6 col-lg-4"
+                data-aos="fade-up"
+                data-aos-delay="200"
+              >
+                <a href="images/tc2.jpg" class="media-1 glightbox">
+                  <img src="images/tc2.jpg" alt="Image" class="img-fluid" />
+                  <div class="media-1-content">
+                    <h2>Dain Chan</h2>
+                    <span class="category">Mage</span>
+                  </div>
+                </a>
+              </div>
+              <div
+                class="col-md-6 col-lg-4"
+                data-aos="fade-up"
+                data-aos-delay="300"
+              >
+                <a href="images/ca2.jpg" class="media-1 glightbox">
+                  <img src="images/ca2.jpg" alt="Image" class="img-fluid" />
+                  <div class="media-1-content">
+                    <h2>Capheny</h2>
+                    <span class="category">Mage</span>
+                  </div>
+                </a>
+              </div>
+              <div
+                class="col-md-6 col-lg-4"
+                data-aos="fade-up"
+                data-aos-delay="400"
+              >
+                <a href="images/na2.jpg" class="media-1 glightbox">
+                  <img src="images/na2.jpg" alt="Image" class="img-fluid" />
+                  <div class="media-1-content">
+                    <h2>Nataya</h2>
+                    <span class="category">Mage</span>
+                  </div>
+                </a>
+              </div>
+
+              <div
+                class="col-md-6 col-lg-4"
+                data-aos="fade-up"
+                data-aos-delay="100"
+              >
+                <a href="images/zi2.jpg" class="media-1 glightbox">
+                  <img src="images/zi2.jpg" alt="Image" class="img-fluid" />
+                  <div class="media-1-content">
+                    <h2>Zill</h2>
+                    <span class="category">Assassin</span>
+                  </div>
+                </a>
+              </div>
+              <div
+                class="col-md-6 col-lg-4"
+                data-aos="fade-up"
+                data-aos-delay="200"
+              >
+                <a href="images/ma2.jpg" class="media-1 glightbox">
+                  <img src="images/ma2.jpg" alt="Image" class="img-fluid" />
+                  <div class="media-1-content">
+                    <h2>Macloch</h2>
+                    <span class="category">Fighter</span>
+                  </div>
+                </a>
+              </div>
+              <div
+                class="col-md-6 col-lg-4"
+                data-aos="fade-up"
+                data-aos-delay="300"
+              >
+                <a href="images/kk2.jpg" class="media-1 glightbox">
+                  <img src="images/kk2.jpg" alt="Image" class="img-fluid" />
+                  <div class="media-1-content">
+                    <h2>Kriknak</h2>
+                    <span class="category">Assassin</span>
+                  </div>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="section">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-4 pe-lg-5" data-aos="fade-up">
+            <span class="subheading d-inline-block mb-2">Download!</span>
+            <h2 class="mb-4">
+              Arena of valor
+            </h2>
+            <p class="mb-5">Download on appstore or google play!</p>
+            <p><a href="#" class="btn btn-black">Have fun</a></p>
+          </div>
+          <div class="col-lg-8">
+            <div class="row">
+              <div
+                class="col-6 col-lg-6 mt-5"
+                data-aos="fade-up"
+                data-aos-delay="100"
+              >
+                <img
+                  src="images/dra.gif"
+                  alt="Image"
+                  class="img-fluid rounded"
+                />
+              </div>
+              <div
+                class="col-6 col-lg-6"
+                data-aos="fade-up"
+                data-aos-delay="200"
+              >
+                <img
+                  src="images/kok.gif"
+                  alt="Image"
+                  class="img-fluid rounded"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="site-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-4">
+            <div class="widget">
+              <h3>Contact</h3>
+              <address>Bangkok University</address>
+              <ul class="list-unstyled links">
+                <li><a href="tel://11234567890">+1(123)-456-7890</a></li>
+                <li><a href="tel://11234567890">+1(123)-456-7890</a></li>
+                <li>
+                  <a href="https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox"
+                    >patchaporn.sith@bumail.net</a
+                  >
+                </li>
+              </ul>
+            </div>
+            <!-- /.widget -->
+          </div>
+          <!-- /.col-lg-4 -->
+          <div class="col-lg-4">
+            <div class="widget">
+              <h3>Sources</h3>
+              <ul class="list-unstyled float-start links">
+                <li><a href="#">About me</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Vision</a></li>
+                <li><a href="#">Mission</a></li>
+                <li><a href="#">Terms</a></li>
+                <li><a href="#">Privacy</a></li>
+              </ul>
+              <ul class="list-unstyled float-start links">
+                <li><a href="#">Partners</a></li>
+                <li><a href="#">Business</a></li>
+                <li><a href="#">Careers</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">FAQ</a></li>
+                <li><a href="#">Creative</a></li>
+              </ul>
+            </div>
+            <!-- /.widget -->
+          </div>
+          <!-- /.col-lg-4 -->
+          <div class="col-lg-4">
+            <div class="widget">
+              <h3>Links</h3>
+              <ul class="list-unstyled links">
+                <li><a href="#">Our Vision</a></li>
+                <li><a href="#">About me</a></li>
+                <li><a href="#">Contact me</a></li>
+              </ul>
+
+              <ul class="list-unstyled social">
+                <li>
+                  <a href="#"><span class="icon-instagram"></span></a>
+                </li>
+                <li>
+                  <a href="#"><span class="icon-twitter"></span></a>
+                </li>
+                <li>
+                  <a href="#"><span class="icon-facebook"></span></a>
+                </li>
+                <li>
+                  <a href="#"><span class="icon-linkedin"></span></a>
+                </li>
+                <li>
+                  <a href="#"><span class="icon-pinterest"></span></a>
+                </li>
+                <li>
+                  <a href="#"><span class="icon-dribbble"></span></a>
+                </li>
+              </ul>
+            </div>
+            <!-- /.widget -->
+          </div>
+          <!-- /.col-lg-4 -->
+        </div>
+        <!-- /.row -->
+
+        <div class="row mt-5">
+          <div class="col-12 text-center">
+            <p class="mb-0">
+              Copyright &copy;
+
+              . All Rights Reserved. &mdash; Be a good players with
+              <a href="https://www.facebook.com/mercury.notyours">Patchaporn</a>
+              <!-- License information: https://www.facebook.com/mercury.notyours/ -->
+            </p>
+          </div>
+        </div>
+      </div>
+      <!-- /.container -->
+    </div>
+    <!-- /.site-footer -->
+  </div>
+</template>
+
+
+
+<script>
+import Navbar from '../components/Navbar.vue'
+export default {
+    components: { Navbar },
+    data() { 
+        return { 
+            name: 'Patchaporn Siththichoi',
+            motto: 'Play to win',
+
+        } }
+};
+</script>
+
+<style>
+</style>
---
+++ src/pages/portfolio.vue
@@ -0,0 +1,196 @@
+<template>
+  <div>
+    <Navbar />
+ <!-- home section starts  -->
+
+	<div class="hero v-h-full overlay">
+		<div class="img-bg rellax">
+			<img src="images\dra.gif" class="width-complete" alt="...">
+		</div>
+		<div class="container">
+			<div class="row align-items-center v-h-full">
+
+				<div class="col-lg-7 col-xxl-6 mx-auto text-center">
+					<h1 class="heading-2 text-white mb-3" data-aos="fade-up">How to play</h1>
+					<p data-aos="fade-up" data-aos-delay="200" class="text-white lead mb-4">Arena of valor</p>
+					<p data-aos="fade-up" data-aos-delay="300"><a href="index.html" class="btn btn-primary-outline white me-4">ROV</a></p>
+				</div>
+
+			</div>
+		</div>
+
+	</div>
+
+
+
+
+	<div class="section">
+		<div class="container">
+			<div class="row justify-content-center mb-5">
+				<div class="col-lg-6 text-center" data-aos="fade-up" data-aos-delay="100">
+					<span class="subheading mb-2 d-inline-block">Arena of valor</span>
+					<h2 class="mb-4">How to play</h2>
+				</div>
+			</div>
+	
+			<div class="row">
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col-md-6 col-lg-4" data-aos="fade-up" data-aos-delay="200">
+							<a href="images/map.png" class="media-1 glightbox">
+								<img src="images/map.png" alt="Image" class="img-fluid">
+								<div class="media-1-content">
+									<h2>Monsters</h2>
+									<span class="category">Monsters are creatures. There are various types of monsters available in the forest, each one will give you money. And different buffs that give our heroes special abilities. for a period of time</span>
+								</div>
+							</a>
+						</div>
+						<div class="col-md-6 col-lg-4" data-aos="fade-up" data-aos-delay="300">
+							<a href="images/map2.png" class="media-1 glightbox">
+								<img src="images/map2.png" alt="Image" class="img-fluid">
+								<div class="media-1-content">
+									<h2>Jungle</h2>
+									<span class="category">The forest will be divided into 2 parts, the forest on our side. and the forest on the enemy side, where if we are on the opposite side The field of view will remain the same unless the Dark Slayer, Abyssal Deagon and Spirit Sentinel switch sides.</span>
+								</div>
+							</a>
+						</div>
+						<div class="col-md-6 col-lg-4" data-aos="fade-up" data-aos-delay="400">
+							<a href="images/map1.png" class="media-1 glightbox">
+								<img src="images/map1.png" alt="Image" class="img-fluid">
+								<div class="media-1-content">
+									<h2>Lane</h2>
+									<span class="category">
+Laneing is very important if you choose to stand in a position that doesn't fit your hero type. It may be a disadvantage to the enemy. and be defeated</span>
+								</div>
+							</a>
+						</div>
+	
+						<div class="col-md-6 col-lg-4" data-aos="fade-up" data-aos-delay="100">
+							<a href="images/base.png" class="media-1 glightbox">
+								<img src="images/base.png" alt="Image" class="img-fluid">
+								<div class="media-1-content">
+									<h2>Base</h2>
+									<span class="category">Each side has a total of 1 base, 9 towers, divided into 3 lanes, each lane has 3 towers, which are outer turrets, middle turrets, and inner turrets. If attacking an enemy tower breaks The whole team earns experience and money.</span>
+								</div>
+							</a>
+						</div>
+						<div class="col-md-6 col-lg-4" data-aos="fade-up" data-aos-delay="200">
+							<a href="images/win.jpg" class="media-1 glightbox">
+								<img src="images/win.jpg" alt="Image" class="img-fluid">
+								<div class="media-1-content">
+									<h2>Score</h2>
+									<span class="category">victory</span>
+								</div>
+							</a>
+						</div>
+						<div class="col-md-6 col-lg-4" data-aos="fade-up" data-aos-delay="300">
+							<a href="images/load.jpg" class="media-1 glightbox">
+								<img src="images/load.jpg" alt="Image" class="img-fluid">
+								<div class="media-1-content">
+									<h2>Loading</h2>
+									<span class="category">This is a page of game loading</span>
+								</div>
+							</a>
+						</div>
+	
+	
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+
+	
+
+	
+
+
+
+	<div class="site-footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-4">
+					<div class="widget">
+						<h3>Contact</h3>
+						<address>Bangkok University</address>
+						<ul class="list-unstyled links">
+							<li><a href="tel://11234567890">+1(123)-456-7890</a></li>
+							<li><a href="tel://11234567890">+1(123)-456-7890</a></li>
+							<li><a href="patchaporn.sith@bumail.net">patchaporn.sith@bumail.net</a></li>
+						</ul>
+					</div> <!-- /.widget -->
+				</div> <!-- /.col-lg-4 -->
+				<div class="col-lg-4">
+					<div class="widget">
+						<h3>Sources</h3>
+						<ul class="list-unstyled float-start links">
+							<li><a href="#">About us</a></li>
+							<li><a href="#">Services</a></li>
+							<li><a href="#">Vision</a></li>
+							<li><a href="#">Mission</a></li>
+							<li><a href="#">Terms</a></li>
+							<li><a href="#">Privacy</a></li>
+						</ul>
+						<ul class="list-unstyled float-start links">
+							<li><a href="#">Partners</a></li>
+							<li><a href="#">Business</a></li>
+							<li><a href="#">Careers</a></li>
+							<li><a href="#">Blog</a></li>
+							<li><a href="#">FAQ</a></li>
+							<li><a href="#">Creative</a></li>
+						</ul>
+					</div> <!-- /.widget -->
+				</div> <!-- /.col-lg-4 -->
+				<div class="col-lg-4">
+					<div class="widget">
+						<h3>Links</h3>
+						<ul class="list-unstyled links">
+							<li><a href="#">Our Vision</a></li>
+							<li><a href="#">About us</a></li>
+							<li><a href="#">Contact us</a></li>
+						</ul>
+
+						<ul class="list-unstyled social">
+							<li><a href="#"><span class="icon-instagram"></span></a></li>
+							<li><a href="#"><span class="icon-twitter"></span></a></li>
+							<li><a href="#"><span class="icon-facebook"></span></a></li>
+							<li><a href="#"><span class="icon-linkedin"></span></a></li>
+							<li><a href="#"><span class="icon-pinterest"></span></a></li>
+							<li><a href="#"><span class="icon-dribbble"></span></a></li>
+						</ul>
+					</div> <!-- /.widget -->
+				</div> <!-- /.col-lg-4 -->
+			</div> <!-- /.row -->
+
+			<div class="row mt-5">
+          <div class="col-12 text-center">
+            <p class="mb-0">
+              Copyright &copy;
+
+              . All Rights Reserved. &mdash; Be a good players with
+              <a href="https://www.facebook.com/mercury.notyours">Patchaporn</a>
+              <!-- License information: https://www.facebook.com/mercury.notyours/ -->
+            </p>
+          </div>
+        </div>
+      </div>
+      <!-- /.container -->
+    </div>
+    <!-- /.site-footer -->
+  </div>
+  
+</template>
+
+
+
+<script>
+import Navbar from '../components/Navbar.vue'
+export default {
+    components: { Navbar }
+};
+</script>
+
+<style>
+</style>
